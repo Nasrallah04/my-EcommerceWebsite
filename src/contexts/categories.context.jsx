4a2c5061a80409236c09ec4d7fa50c51179c3600
Nasrallah04 <nasrallah.dev@gmail.com>
2023-12-04 15:42:53 +0100

    Category Page

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,28 +1,28 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from 'react';
 
-import {getCategoriesAndDocuments} from '../utils/firebase/firebase'
-
-// import SHOP_DATA from "../shop-data.js";
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
 export const CategoriesProvider = ({ children }) => {
-  // You can import the shop data from the external file
-
   const [categoriesMap, setCategoriesMap] = useState({});
-  
+
   useEffect(() => {
-    const getCategorieMap = async () => {
-      const categorieMap = await getCategoriesAndDocuments()
-      console.log(categorieMap)
-      setCategoriesMap(categorieMap)
-    }
-    getCategorieMap()
-  }
-  , []); 
-  
-  const value = { categoriesMap};
-  return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>;
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,23 +1,16 @@
-// import Shop_Data from "../../shop-data";
-import { useContext } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
-import "./shop.styles.scss";
+import { Routes, Route } from 'react-router-dom';
+
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+
+import './shop.styles.scss';
+import Category from '../category/category.component';
+
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <>
-          <h2>{title}</h2>
-          <div key={title} className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </>
-      ))}
-    </>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category/>} />
+    </Routes>
   );
 };
 
