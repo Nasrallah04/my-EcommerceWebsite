a74b63c166638b8c8745a03d795691a59acf541d
nasrallahilyass <nasrallah.dev@gmail.com>
2024-04-18 16:14:26 +0100

    UseReducer: user.context.jsx

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,20 +1,48 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import {
   onAuthStateChangedListner,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase";
+
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+export const INITIAL_STATE = {
+    currentUser: null,
+    };
+
+const userReducer = (state, action) => {
+  console.log("Dispatched")
+  console.log(action)
+  const {type, payload} = action;
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+        return { 
+            ...state,
+             currentUser: payload };
+        default: 
+        throw new Error(`Unhandled type: ${type}`);
+    }
+    };
+
+
 // Start working with Reducers
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+//   const [currentUser, setCurrentUser] = useState(null);
+//   const value = { currentUser, setCurrentUser };
+const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+const {currentUser} = state;
+console.log(currentUser)
+const setCurrentUser = (user) => {
+    dispatch({ type: 'SET_CURRENT_USER', payload: user });
+  }
 
-  // Remove the following line, as it signs the user out when the component initializes
-  // signOutUser();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListner((user) => {
@@ -27,5 +55,5 @@ export const UserProvider = ({ children }) => {
     return unsubscribe;
   }, []);
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={{...state,setCurrentUser}}>{children}</UserContext.Provider>;
 };
