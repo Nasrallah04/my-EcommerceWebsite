c4502feff2e0f2018a6681bf273457029e6b8492
nasrallahilyass <nasrallah.dev@gmail.com>
2024-04-20 22:50:08 +0100

    Creating root-reducer.jsx

---
+++ src/store/store.jsx
@@ -0,0 +1,6 @@
+// Here we'll store all the reducers and the store configuration
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+//root reducer
