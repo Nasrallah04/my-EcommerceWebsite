80bf1ec664f6a3b1e9ac565795b2eecc70cd31eb
nasrallahilyass <nasrallah.dev@gmail.com>
2024-04-21 13:48:30 +0100

    useSelector and create user.selectore.jsx

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,18 +1,22 @@
+import { useSelector } from "react-redux";
 import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import CrownLogo from "../../assets/crown.svg";
 
+
 import CartIcon from "../../components/cart-icon/cart-icon.component";
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/card.context";
 import "./navigation.styles.jsx";
+import { currentUserSelctor } from "../../store/user/user.selector.jsx";
 
 import { signOutUser } from "../../utils/firebase/firebase";
 import CartDropDown from "../../components/cart-dropdown/cart-dropdown.component";
 import { NavigationContainer, LogoContainer, NavLinksContainer, NavLink } from "./navigation.styles.jsx";
 
 function Navigation() {
-  const { currentUser } = useContext(UserContext);
+  // const { currentUser } = useContext(UserContext);\
+  // useSelector it's a function that takes state as an argument and returns the part of the state that you want.
+  const currentUser = useSelector(currentUserSelctor);
   const { isCartOpen } = useContext(CartContext);
 
   return (
