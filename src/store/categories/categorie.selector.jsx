d4f24294fe1cd7f69716954e583a600dbdfb2328
nasrallahilyass <nasrallah.dev@gmail.com>
2024-04-23 10:41:32 +0100

    Memorization in Redux with reselect

--- src/store/categories/categorie.selector.jsx
+++ src/store/categories/categorie.selector.jsx
@@ -1,9 +1,23 @@
-export const categoriesMapSelctor = (state) =>
-{ 
-    console.log("Selctor fired")
-    return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {})};
+import { createSelector } from 'reselect';
+// Memorization:
+// is the process when we cache the result of the previous value and if the input is the same we return the cached value
+const selectCategoryReducer = (state) => state.categories;
+
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) =>categoriesSlice.categories
+);
+
+
+
+export const categoriesMapSelctor = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+  
+)
 // here where you do the transformation logic to get the data in the format you want
