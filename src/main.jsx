2060bf2f3cdc533597758cc88169a5403fe9e46c
nasrallahilyass <nasrallah.dev@gmail.com>
2024-04-23 13:11:45 +0100

    App: v3.0

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,13 @@ import { store } from "./store/store.jsx";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-// import { UserProvider } from "./contexts/user.context.jsx";
-// import { CategoriesProvider } from "./contexts/categories.context.jsx";
-// import { CartProvider } from "./contexts/card.context.jsx";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-      {/* <UserProvider> */}
-        {/* <CategoriesProvider> */}
-          {/* <CartProvider> */}
             <App />
-          {/* </CartProvider> */}
-        {/* </CategoriesProvider> */}
-      {/* </UserProvider> */}
     </BrowserRouter>
   </Provider>
   </React.StrictMode>
