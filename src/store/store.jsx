2684d57d690357fc1d7378cdcb06906e196344aa
nasrallahilyass <nasrallah.dev@gmail.com>
2024-04-24 11:42:32 +0100

    Optimize the logger to work only on development

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -13,23 +13,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-      return next(action);
-    }
-  
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-  
-    next(action);
-  
-    console.log('next state: ', store.getState());
-  };
-
-const middleware = [loggerMiddleware];
-const composedEnhancers = compose(applyMiddleware(...middleware));
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean); // filter out the falsy values
+const composedEnhancers = compose(applyMiddleware(...middlewares));
 
 // Create the store
 // it takes three arguments: the root reducer, the initial state, and the enhancer
