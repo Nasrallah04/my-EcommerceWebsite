b722909fd237b4256142e41c2cd76afbeb441d39
nasrallahilyass <nasrallah.dev@gmail.com>
2024-04-25 10:50:16 +0100

    Redux-thunk :update the reducer categories to handel success,failed, error

--- src/store/categories/categorie.reducer.jsx
+++ src/store/categories/categorie.reducer.jsx
@@ -9,10 +9,22 @@ export const CATEGORIES_INITIAL_STATE = {
 export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {}) => {
     const {type, payload} = action;
     switch (type) {
-        case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+            return {
+                ...state,
+                isLoading: true,
+            };
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: payload,
+            };
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
             return {
                 ...state,
                 categories: payload,
+                isLoading: false,
             };
         default:
             return state;
