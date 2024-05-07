26e58a51654f2717d28dd8250cfcc6a5b10931e2
nasrallahilyass <nasrallah.dev@gmail.com>
2024-05-07 12:36:57 +0100

    Update keys name

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { store, persistor } from "./store/store.jsx";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import {Elements} from '@stripe/react-stripe-js';
+import { stripePromise } from "./utils/strip/strip.utils.jsx";
 import App from "./App.jsx";
 import "./index.scss";
 
@@ -13,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
   <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
-      <Elements>
+      <Elements stripe={stripePromise}>
             <App />
       </Elements>
     </BrowserRouter>
