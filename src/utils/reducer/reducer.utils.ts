1e751c669e025dc5d4869219f9e83f8c4b0b37f2
nasrallahilyass <nasrallah.dev@gmail.com>
2024-05-15 13:02:40 +0100

    Type Predicate Functions

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,5 +1,25 @@
 import { AnyAction } from "redux-saga";
 
+
+type Aline = {
+    fly: boolean
+}
+
+type Human = {
+    speak: () => {}
+}
+
+function isHuman(entity: Aline | Human): entity is Human {
+    return (entity as Human).speak !== undefined
+}
+
+const Ilyass
+
+if(isHuman(Ilyass)) {
+    Ilyass.speak()
+}
+
+
 export type ActionTypeWithPayload<T, P> = {
     type: T,
     payload: P
@@ -8,8 +28,6 @@ export type ActionTypeWithPayload<T, P> = {
 export type ActionType<T> = {
     type: T 
 }
-
-
 
 export function createAction<T extends string, P>(type: T, payload: P) : ActionTypeWithPayload<T, P>
 
