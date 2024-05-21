da4c16512cf12989bdece75630a39b25b2946184
nasrallahilyass <nasrallah.dev@gmail.com>
2024-05-21 10:52:42 +0100

    typing our categorie Saga

--- src/store/categories/categorie.saga.ts
+++ src/store/categories/categorie.saga.ts
@@ -1,5 +1,5 @@
 // Moving the fetchCategoriesAsync function from thunk to saga:
-import { takeLatest, call, put, all } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "typed-redux-saga";
 import {getCategoriesAndDocuments} from '../../utils/firebase/firebase';
 import {fetchCategoriesSuccess, fetchCategoriesFailure} from './categorie.action';
 import {CATEGORIES_ACTION_TYPES} from './categorie.types';
@@ -7,20 +7,20 @@ import {CATEGORIES_ACTION_TYPES} from './categorie.types';
 
 export function* fetchCategoriesAsync() {
     try {
-        const categoriesArray = yield call(getCategoriesAndDocuments, 'categories' );
-        yield put(fetchCategoriesSuccess(categoriesArray));
+        const categoriesArray = yield* call(getCategoriesAndDocuments, 'categories' );
+        yield* put(fetchCategoriesSuccess(categoriesArray));
     } catch (error) {
-        yield put(fetchCategoriesFailure(error))
+        yield* put(fetchCategoriesFailure(error as Error))
     }
 
 }
 
 export function* onFetchCategories() {
-    yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync)
+    yield* takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync)
 
 }
 
 export function* categoriesSaga() {
-    yield all([call(onFetchCategories)])
+    yield* all([call(onFetchCategories)])
 
 }
